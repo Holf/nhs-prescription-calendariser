d1fded5ed745c90401b9104c29dc62771af75d44
Holf <holf@holf.net>
2023-09-18 20:28:12 +0100

    Removed unused test files

--- reportGeneration/src/reporting/getMedicationStockCount/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { assert, describe, it } from "@src/deps.ts";
-import { getMedicationStockCount } from "@src/reporting/getMedicationStockCount/index.ts";
-
-describe("getMedicationStockCount", () => {
-  it("more stuff", () => {
-    //assert(Number.isNaN(getMedicationStockCount("billy")));
-  });
-});
